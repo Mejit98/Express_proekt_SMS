b60114f8f9771af88286fe3ccf05a9f24911e2f4
Mejit98 <medzitssejthalilov@gmail.com>
2025-10-19 12:14:34 +0300

    scooby_4_3

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,47 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* Страница Фреда*/
+/* Страница Фреда */
 router.get('/fred', function(req, res, next) {
-    res.send("<h1>Страница Фреда</h1>")
+    res.render('scoob', {
+        title: "Фред",
+        picture: "images/fred.png",
+        desc: "Лидер команды. Смелый, решительный и любит разрабатывать планы по поимке злодеев. Обычно он за рулём фургона «Тайна-машина» и часто берёт на себя руководство расследованиями."
+    });
 });
-/* Страница Давны*/
-router.get('/dafna', function(req, res, next) {
-    res.send("<h1>Страница Давны</h1>")
+/* Страница Скуби */
+router.get('/scooby-dy', function(req, res, next) {
+    res.render('scoob', {
+        title: "Скуби-Ду",
+        picture: "images/scooby-dy.png",
+        desc: "Добродушный, но очень трусливый пёс. Несмотря на страх, всегда помогает друзьям. Любит еду и особенно обожает «Скуби-снеки»."
+    });
+});
+
+/* Страница Шэгги */
+router.get('/sheki', function(req, res, next) {
+    res.render('scoob', {
+        title: "Шэгги",
+        picture: "images/sheki.jpeg",
+        desc: "Лучший друг Скуби-Ду. Весёлый и немного трусоватый парень, который постоянно думает о еде. Часто оказывается в забавных ситуациях вместе со Скуби."
+    });
 });
 
 /* Страница Велмы */
 router.get('/velma', function(req, res, next) {
-    res.send("<h1>Страница Велмы</h1>")
-});
-/* Страница Шегги */
-router.get('/sheki', function(req, res, next) {
-    res.send("<h1>Страница Шегги</h1>")
-});
-
-/* Страница Скуби-ду */
-router.get('/scooby-dy', function(req, res, next) {
-    res.send("<h1>Страница Скуби-ду</h1>")
+    res.render('scoob', {
+        title: "Велма",
+        picture: "images/velma.png",
+        desc: "Самая умная в команде. Логичная, рассудительная и наблюдательная. Благодаря ей команда часто раскрывает все тайны."
+    });
 });
 
-
+/* Страница Дафны */
+router.get('/dafna', function(req, res, next) {
+    res.render('scoob', {
+        title: "Дафна",
+        picture: "images/dafna.png",
+        desc: "Стильная и энергичная участница команды. Всегда готова помочь друзьям и не боится трудностей. Отличается добротой и находчивостью."
+    });
+});
 module.exports = router;
