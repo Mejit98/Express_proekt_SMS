96ff6499a4ddde5fab14b3928fb91d7169a52d67
Mejit98 <medzitssejthalilov@gmail.com>
2025-10-19 11:35:45 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,28 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Страница Фреда*/
+router.get('/fred', function(req, res, next) {
+    res.send("<h1>Страница Фреда</h1>")
+});
+/* Страница Давны*/
+router.get('/dafna', function(req, res, next) {
+    res.send("<h1>Страница Давны</h1>")
+});
+
+/* Страница Велмы */
+router.get('/velma', function(req, res, next) {
+    res.send("<h1>Страница Велмы</h1>")
+});
+/* Страница Шегги */
+router.get('/sheki', function(req, res, next) {
+    res.send("<h1>Страница Шегги</h1>")
+});
+
+/* Страница Скуби-ду */
+router.get('/scooby-dy', function(req, res, next) {
+    res.send("<h1>Страница Скуби-ду</h1>")
+});
+
 
 module.exports = router;
