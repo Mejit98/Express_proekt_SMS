47fd8c7e9f9d381201756a0e474b4c10568bab18
Mejit98 <medzitssejthalilov@gmail.com>
2025-11-27 08:35:08 +0300

    scooby_7_2

--- scoobyMongoose.js
+++ scoobyMongoose.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://127.0.0.1:27017/scoobyMongoose2025');
 
-const Scooby = mongoose.model('Scooby', { name: String });
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.gav = function(){
+   console.log(this.name + " сказал гав")
+}
+
+const Scooby = mongoose.model('Scooby', schema);
+
 const scoobyEntry = new Scooby({ name: 'Скуби-ду' });
-scoobyEntry.save().then(() => console.log('Гав'));
+
+scoobyEntry.save().then(() => scoobyEntry.gav());
 
