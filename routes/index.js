5605db29f9a6811f620ffe47e65c1179094a17e7
Mejit98 <medzitssejthalilov@gmail.com>
2025-12-21 13:06:05 +0300

    scooby_10_1

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    req.session.greeting = "Hi!!!";
     res.render('index', { title: 'Express',  counter:req.session.counter });
 });
+
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+ res.render('logreg',{title: 'Вход'});
+ });
+
 
 
 module.exports = router;
