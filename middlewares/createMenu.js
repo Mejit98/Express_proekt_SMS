32d396dee10485b4ad3d9250611c667971eef18a
Mejit98 <medzitssejthalilov@gmail.com>
2025-12-21 12:37:33 +0300

    scooby_9_5

---
+++ middlewares/createMenu.js
@@ -0,0 +1,13 @@
+var Scooby = require("../models/scooby-du").Scooby
+
+module.exports = async function(req,res,next){
+   res.locals.nav = []
+
+
+  var menu =  await Scooby.find(null,{_id:0,title:1,nick:1});
+  console.log(menu);
+  if (menu.length != 0) {
+   res.locals.nav = menu;
+  }
+  next();
+}
