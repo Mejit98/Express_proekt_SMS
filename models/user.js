b721db7b27cc615980ac56ad4f3cf9a1fd5a4a28
Mejit98 <medzitssejthalilov@gmail.com>
2025-12-21 14:46:20 +0300

    scooby_10_4

--- models/user.js
+++ models/user.js
@@ -33,6 +33,8 @@ userSchema.virtual("password").set(function(password){
 userSchema.methods.encryptPassword = function(password){
     return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
 }
-
+userSchema.methods.checkPassword = function(password){
+    return this.hashedPassword === this.encryptPassword(password);
+}
 
 module.exports.User = mongoose.model("User", userSchema)
