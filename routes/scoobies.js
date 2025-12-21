d5aeab5f158bceb85082d59f85d73fda7dcac78a
Mejit98 <medzitssejthalilov@gmail.com>
2025-12-21 16:06:47 +0300

    scooby_10_8

--- routes/scoobies.js
+++ routes/scoobies.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var createError = require('http-errors');
 var Scooby = require('../models/scooby-du').Scooby;
+var checkAuth = require("../middlewares/checkAuth.js");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,7 +10,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница корпорации тайна */
-router.get("/:nick", async function(req, res, next) {
+router.get("/:nick", checkAuth, async function(req, res, next) {
     var scoobies = await Scooby.find({nick: req.params.nick});
     console.log(scoobies)
     if(!scoobies.length) return next(new Error("Нет такого персонажа в мультсериале Корпорация тайна"))
